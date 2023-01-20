bf685c7de4671c8190a1e7e4c529d5187baf0a4a
JNK <deal6789@naver.com>
2023-01-20 18:45:09 +0900

    10.4 10.5

--- mission.py
+++ mission.py
@@ -1,22 +1,25 @@
-# 10.1
+'''
+10.4 name, symbol, number 인스턴스 속성 가진
+ Element 클래스를 만들어보자.
+ 이 클래스에서 'Hydrogen', 'H', 1 값을 가진 객체를 생성한다.
 
-class Thing():
-    pass
-print(Thing)
+'''
 
-example = Thing()
-print(example)
+class Element():
+    def __init__(self, name, symbol, number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
 
-# 10.2
+produce = Element('Hydrogen', 'H', 1)
+print(produce.name, produce.symbol, produce.number)
 
-class Thing2():
-    letters='abc'
-print(Thing2.letters)
+'''
+10.5 'name' : 'Hydrogen', 'symbol' : 'H', 'number' : 1과 같이
+ 키와 값으로 이루어진 el_dict 딕셔너리를 만들어보자.
+ 그리고 el_dict 딕셔너리로부터 Element 클래스의 hydrogen 객체를 생성한다.
+'''
 
-# 10.3
-
-class Thing3():
-    def __init__(self):
-        self.letters='xyz'
-example=Thing3()
-print(example.letters)
+el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
+hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
+print(hydrogen.name, hydrogen.symbol, hydrogen.number)
