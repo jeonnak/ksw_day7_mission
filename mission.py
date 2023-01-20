1badbef2b6b72240b83622b93b1c08735aa79c3a
JNK <deal6789@naver.com>
2023-01-20 18:45:53 +0900

    10.6

--- mission.py
+++ mission.py
@@ -1,25 +1,18 @@
 '''
-10.4 name, symbol, number 인스턴스 속성 가진
- Element 클래스를 만들어보자.
- 이 클래스에서 'Hydrogen', 'H', 1 값을 가진 객체를 생성한다.
-
+ 10.6 Element 클래스에서 객체의 속성(name, symbol, number) 값을
+ 출력하는 dump() 메서드를 정의한다. 이 클래스의 hydrogen 객체를
+ 생성하고, dump() 메서드로 이 속성을 출력한다.
 '''
+
 
 class Element():
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
-
-produce = Element('Hydrogen', 'H', 1)
-print(produce.name, produce.symbol, produce.number)
-
-'''
-10.5 'name' : 'Hydrogen', 'symbol' : 'H', 'number' : 1과 같이
- 키와 값으로 이루어진 el_dict 딕셔너리를 만들어보자.
- 그리고 el_dict 딕셔너리로부터 Element 클래스의 hydrogen 객체를 생성한다.
-'''
+    def dump(self):
+        print(self.name, self.symbol, self.number)
 
 el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
 hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
-print(hydrogen.name, hydrogen.symbol, hydrogen.number)
+hydrogen.dump()
