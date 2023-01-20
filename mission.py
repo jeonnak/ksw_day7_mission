51453b16d93859d6fad4b19544c30212d6e8976d
JNK <deal6789@naver.com>
2023-01-20 18:46:44 +0900

    10.7

--- mission.py
+++ mission.py
@@ -1,18 +1,18 @@
 '''
- 10.6 Element 클래스에서 객체의 속성(name, symbol, number) 값을
- 출력하는 dump() 메서드를 정의한다. 이 클래스의 hydrogen 객체를
- 생성하고, dump() 메서드로 이 속성을 출력한다.
+ 10.7 print(hydrogen)을 호출한다. Element 클래스의 정의에서 dump 메서드를
+ __str__() 메서드로 바꿔서 새로운 hydrogen 객체를 생성하고,
+ 그리고 print(hydrogen)을 다시 호출한다.
 '''
-
 
 class Element():
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
-    def dump(self):
-        print(self.name, self.symbol, self.number)
+    def __str__(self):
+        return f'{self.name} {self.symbol} {self.number}'
 
 el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
 hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
-hydrogen.dump()
+
+print(hydrogen)
